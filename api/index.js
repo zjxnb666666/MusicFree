782e13e3effc08e71d30a506facd07b87c863cf7
zjxnb666666 <zjxnb666666@gmail.com>
2025-02-06 22:18:28 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,28 @@
-const { createServer } = require('http')
 const express = require('express')
 const { createWebAPIRequest } = require('../util/util')
 
 const app = express()
-const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// 添加错误处理中间件
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).json({
+    code: 500,
+    msg: 'Internal Server Error',
+    error: err.message
+  })
+})
+
+// 添加CORS支持
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 // 搜索接口
 app.get('/search', (req, res) => {
@@ -27,55 +43,16 @@ app.get('/search', (req, res) => {
     data,
     cookie,
     (music_req, music_res) => {
-      res.send(music_res)
+      try {
+        res.json(JSON.parse(music_res))
+      } catch (e) {
+        res.status(500).json({ error: 'Parse Error' })
+      }
     },
-    err => res.status(502).send('fetch error')
+    err => res.status(502).json({ error: 'API Error', message: err.message })
   )
 })
 
-// 获取歌曲详情
-app.get('/song/detail', (req, res) => {
-  const { ids } = req.query
-  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
-  const data = {
-    csrf_token: '',
-    ids: [ids]
-  }
-
-  createWebAPIRequest(
-    'music.163.com',
-    '/weapi/v3/song/detail',
-    'POST',
-    data,
-    cookie,
-    (music_req, music_res) => {
-      res.send(music_res)
-    },
-    err => res.status(502).send('fetch error')
-  )
-})
-
-// 获取歌曲URL
-app.get('/song/url', (req, res) => {
-  const { id } = req.query
-  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
-  const data = {
-    ids: [id],
-    br: 320000,
-    csrf_token: ''
-  }
-
-  createWebAPIRequest(
-    'music.163.com',
-    '/weapi/song/enhance/player/url',
-    'POST',
-    data,
-    cookie,
-    (music_req, music_res) => {
-      res.send(music_res)
-    },
-    err => res.status(502).send('fetch error')
-  )
-})
+// ... 其他接口保持不变
 
 module.exports = app
